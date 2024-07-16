e6d6bd32a1264c296e9b2d992f020c71cfdf665c
Pietrell <piotrplotast2006@gmail.com>
2024-07-16 13:30:17 +0200

    add font

--- rock-paper-scissors-game/tailwind.config.js
+++ rock-paper-scissors-game/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gradient-start": "#1F3757",
+        "gradient-end": "#131537",
+      },
+      fontFamily: {
+        main: ["Barlow Semi Condensed", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
